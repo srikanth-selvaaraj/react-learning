b2cd70c162e1d15836600660cf28d6813d943c1f
srikanth-selvaaraj <srikanth.selvaaraj@gmail.com>
2023-11-17 13:17:56 +0700

    removed all the files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Php  from './Php';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// baic element
+const myFirstElement = <h1 className='title'>Hello React!, My age is {10 + 10}</h1>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// table - Need one top level tag
+const table = (
+    <table>
+        <tr>
+            <th>Name</th>
+            <th>Age</th>
+        </tr>
+        <tr>
+            <td>Srikanth</td>
+            <td>20</td>
+        </tr>
+    </table>
+)
+
+const root = ReactDOM.createRoot(document.getElementById('table'));
+root.render(<Php color='red' skin='white'/>)
+// root.render(myFirstElement)
